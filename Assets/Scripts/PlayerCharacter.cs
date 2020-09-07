691ab525a2824f273a9d7cb625fe9d2b8edb927d
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2020-09-08 00:21:37 +0300

    Add Menu

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+	private int _helth;
+    void Start()
+    {
+		_helth = 5;
+    }
+
+	public void Hurt(int damage)
+	{
+		_helth -= damage;
+	}
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text _scorelable;
+
+    void Update()
+    {
+        _scorelable.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OpenSettings()
+    {
+    }
+
+    public void OnPointerDown()
+    {
+        Debug.LogError("Pointer down");
+    }
+}
