6cc6800653d864f4ba6b94895cae2444d156d621
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2020-09-07 22:09:29 +0300

    first commit

---
+++ Assets/WanderingAI.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+	[SerializeField] private GameObject fireballPrefab;
+	private GameObject _fireball;
+	public float speed = 3.0f;
+	public float obstracleRange = 5.0f;
+	private bool _alive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	public void SetAlive(bool value)
+	{
+		_alive = value;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (_alive)
+		{
+			transform.Translate(0, 0, speed * Time.deltaTime);
+			Ray ray = new Ray(transform.position, transform.forward);
+			RaycastHit hit;
+			if (Physics.SphereCast(ray, 0.75f, out hit))
+			{
+				GameObject hitObject = hit.transform.gameObject;
+				if(hitObject.GetComponent<PlayerCharacter>())
+				{
+					Debug.Log("Fireball");
+
+					if (_fireball == null)
+					{
+					//	Debug.Log("Fireball");
+						_fireball = Instantiate(fireballPrefab) as GameObject;
+						_fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+						_fireball.transform.rotation = transform.rotation;
+					}
+				}
+				else if (hit.distance < obstracleRange)
+				{
+					float angle = Random.Range(-110, 110);
+					transform.Rotate(0, angle, 0);
+				}
+				
+			}
+		}
+    }
+}
